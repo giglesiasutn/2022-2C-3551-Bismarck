34a9fd20e5738970c9d5f1542f5aa9c9113f99d3
Gabriel Iglesias <giglesias@frba.utn.edu.ar>
2022-09-11 14:44:00 -0300

    Agrego camara libre. Agrego logica para que se inserten Islas y mapas de forma aleatoria en el mapa junto con su dibujado. Falta corregir la posicion para que no coincida con la de otro modelo en el mapa.

--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Reflection;
+using Bismarck.Cameras;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Color = Microsoft.Xna.Framework.Color;
 
 namespace TGC.MonoGame.TP
 {
@@ -49,8 +53,16 @@ namespace TGC.MonoGame.TP
         private Matrix View { get; set; }
         private Matrix Projection { get; set; }
 
+        private Model ShipBModel { get; set; }
         private Model seaModel { get; set; }
+        private Model IslandModel { get; set; }
         private FollowCamera FollowCamera { get; set; }
+
+        private List<Matrix> IslandWords = new List<Matrix>();
+        private List<Matrix> ShipsBWords = new List<Matrix>();
+        private Camera TestCamera { get; set; }
+
+
         private float CarCurrentSpeed;
         private float rotationY;
 
@@ -72,11 +84,15 @@ namespace TGC.MonoGame.TP
 
             // Configuramos nuestras matrices de la escena.
             ShipWorld = Matrix.Identity;
-            View = Matrix.CreateLookAt(Vector3.UnitZ * 250, Vector3.Zero, Vector3.Up);
+            View = Matrix.CreateLookAt(Vector3.UnitZ * 5000, Vector3.Zero, Vector3.Up);
             Projection =
-                Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 550);
-            // Creo una camara para seguir a nuestro auto
+                Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 500);
+            // Creo una camara para seguir a nuestro varco
             FollowCamera = new FollowCamera(GraphicsDevice.Viewport.AspectRatio);
+            var size = GraphicsDevice.Viewport.Bounds.Size;
+            size.X /= 2;
+            size.Y /= 2;
+            TestCamera = new FreeCamera(GraphicsDevice.Viewport.AspectRatio, new Vector3(0, 50, 1000), size);
 
             // Inicializo velocidad
             /*CarCurrentSpeed = 0;
@@ -98,8 +114,80 @@ namespace TGC.MonoGame.TP
 
             // Cargo el modelo del logo.
             ShipModel = Content.Load<Model>(ContentFolder3D + "Ship/Ship");
+            ShipBModel = Content.Load<Model>(ContentFolder3D + "ShipB/Source/Ship");
+            IslandModel = Content.Load<Model>(ContentFolder3D + "Island1/Island1");
             seaModel = Content.Load<Model>(ContentFolder3D + "Sea/sea_escenario_with_texture");
-            
+
+
+            // Cargo Islands en lugares aleatorios no repetidos
+            Random r = new Random();
+            IslandWords.Add(Matrix.Identity * Matrix.CreateTranslation(new Vector3(0, 0, 0)));
+            for (int i = 0; i < 60; i++)
+            {
+                //Cambio los modelos cada 20
+                if (i == 20)
+                    IslandModel = Content.Load<Model>(ContentFolder3D + "Island2/Island2");
+                if (i == 40)
+                    IslandModel = Content.Load<Model>(ContentFolder3D + "Island2/Island2");
+
+                bool vacio = true;
+                while (vacio)
+                {
+                    
+                    float x = r.Next(0, 200000);
+                    float y = 0;  //Fijo en Y
+                    float z = r.Next(0, 200000);
+                    foreach (Matrix IslandWord in IslandWords)
+                    {
+                        vacio = true;
+                        if (IslandWord.Translation.X == x || IslandWord.Translation.Z == z) {
+                            vacio = false;
+                            break;
+                        }
+                    }
+                    if (vacio == true) {
+                        IslandWords.Add(Matrix.Identity * Matrix.CreateTranslation(new Vector3(x, y, z)));
+                        vacio = false; //Salgo del bucle hay que mejorar esto
+                    }
+
+                }
+            }
+            // Cargo Ships en posiciones aleatorias
+             
+            ShipsBWords.Add(Matrix.Identity * Matrix.CreateTranslation(new Vector3(r.Next(1000, 200000), 0, r.Next(1000, 200000))));
+
+            List<Matrix> WordsTotal = new List<Matrix>(IslandWords);
+
+            for (int i = 0; i < 40; i++)
+            {
+                bool vacio = true;
+                while (vacio)
+                {
+
+                    float x = r.Next(0, 50000);
+                    float y = 0;  //Fijo en Y
+                    float z = r.Next(0, 50000);
+                    //Verifico aqui, con el total de vectores posiciones en el mundo (islas y luego islas y barcos)
+                    foreach (Matrix ShipsBWord in WordsTotal)
+                    {
+                        vacio = true;
+                        if (ShipsBWord.Translation.X == x || ShipsBWord.Translation.Z == z)
+                        {
+                            vacio = false;
+                            break;
+                        }
+                    }
+                    if (vacio == true)
+                    {
+                        ShipsBWords.Add(Matrix.Identity * Matrix.CreateTranslation(new Vector3(x, y, z)));
+                        WordsTotal.Add(Matrix.Identity * Matrix.CreateTranslation(new Vector3(x, y, z)));
+                        vacio = false; //Salgo del bucle hay que mejorar esto
+                    }
+
+                }
+            }
+
+
             // Cargo un efecto basico propio declarado en el Content pipeline.
             // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
             Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
@@ -116,7 +204,7 @@ namespace TGC.MonoGame.TP
         protected override void Update(GameTime gameTime)
         {
             // Aca deberiamos poner toda la logica de actualizacion del juego.
-
+            TestCamera.Update(gameTime);
             // Capturar Input teclado
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 //Salgo del juego.
@@ -125,46 +213,9 @@ namespace TGC.MonoGame.TP
             // Basado en el tiempo que paso se va generando una rotacion.
             Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
             var rotationMatrix = Matrix.CreateRotationY(Rotation);
-            ShipWorld = Matrix.CreateScale(0.05f) * rotationMatrix;
-            // Actualizo la camara, enviandole la matriz de mundo del auto
-            FollowCamera.Update(gameTime, ShipWorld);
+            ShipWorld = Matrix.CreateScale(0.05f) * rotationMatrix ;
 
-            //Movimiento
-            /*
-            float elapsedTime = Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
-            Vector3 previousPosition = CarWorld.Translation;
 
-            //Giros
-            if (keyboardState.IsKeyDown(Keys.D))
-                rotationY += CargAngularSpeed * elapsedTime * -1.0f * CarCurrentSpeed / 4;
-            if (keyboardState.IsKeyDown(Keys.A))
-                rotationY += CargAngularSpeed * elapsedTime * CarCurrentSpeed / 4;
-
-            CarWorld = Matrix.CreateRotationY(rotationY);
-
-            //Aceleracion y desaleracion (+marcha atras)
-            if (keyboardState.IsKeyDown(Keys.W))
-                CarCurrentSpeed += CarAcceleration * elapsedTime;
-            else if (keyboardState.IsKeyDown(Keys.S))
-                CarCurrentSpeed -= CarAcceleration * elapsedTime;
-            else
-            {
-                //Para que desacelere solo al dejar de avanzar o retroceder
-                if (CarCurrentSpeed > 0)
-                    CarCurrentSpeed -= CarAcceleration * elapsedTime;
-                else if (CarCurrentSpeed < 0)
-                    CarCurrentSpeed += CarAcceleration * elapsedTime;
-            }
-
-            if (CarCurrentSpeed > CarMaxSpeed)
-                CarCurrentSpeed = CarMaxSpeed;
-            if (CarCurrentSpeed < CarMinSpeed)
-                CarCurrentSpeed = CarMinSpeed;
-
-            Vector3 movement = CarWorld.Forward * CarCurrentSpeed;
-
-            ShipWorld = ShipWorld * Matrix.CreateTranslation(previousPosition + movement);
-            */
             base.Update(gameTime);
         }
 
@@ -178,12 +229,23 @@ namespace TGC.MonoGame.TP
             GraphicsDevice.Clear(Color.Cyan);
 
             // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
-            Effect.Parameters["View"].SetValue(FollowCamera.View);
-            Effect.Parameters["Projection"].SetValue(FollowCamera.Projection);
+            Effect.Parameters["View"].SetValue(TestCamera.View);
+            Effect.Parameters["Projection"].SetValue(TestCamera.Projection);
             Effect.Parameters["DiffuseColor"].SetValue(Color.Beige.ToVector3());
 
-            ShipModel.Draw(ShipWorld, FollowCamera.View, FollowCamera.Projection);
-            seaModel.Draw(Matrix.CreateScale(10), FollowCamera.View, FollowCamera.Projection);
+            //ShipModel.Draw(ShipWorld, FollowCamera.View, FollowCamera.Projection);
+            ShipModel.Draw(ShipWorld, TestCamera.View, TestCamera.Projection);
+
+            foreach (Matrix IslandWord in IslandWords)
+            {
+                IslandModel.Draw(IslandWord * Matrix.CreateScale(0.3f), TestCamera.View, TestCamera.Projection);
+            }
+            foreach (Matrix ShipsBWord in ShipsBWords)
+            {
+                ShipBModel.Draw(ShipsBWord * Matrix.CreateScale(0.6f), TestCamera.View, TestCamera.Projection);
+            }
+
+            seaModel.Draw(Matrix.CreateScale(10), TestCamera.View, TestCamera.Projection);
         }
 
         /// <summary>
